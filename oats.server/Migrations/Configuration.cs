423a7810ee25a10837f5c61f133a79bfdc34a3f2
jason neill <jtneill@gmail.com>
2014-09-10 08:31:07 -0500

    enabled auto migrations. deployed to azure successful POST to azure sql db.

--- oats.server/Migrations/Configuration.cs
+++ oats.server/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace oats.server.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "OatsServerContext";
         }
 
